a230a5f15fb7dbe47e4dfea28e8940a7c62f0c2f
kalynnka <luhui19970305@hotmail.com>
2026-01-18 16:11:33 +0800

    still isinstance

--- arcanus/base.py
+++ arcanus/base.py
@@ -331,7 +331,7 @@ class BaseTransmuter(BaseModel, metaclass=TransmuterMetaclass):
 
     def __getattribute__(self, name: str) -> Any:
         value = super().__getattribute__(name)
-        if name in type(self).model_associations:
+        if isinstance(value, Association):
             value.prepare(self, name)
         return value
 
